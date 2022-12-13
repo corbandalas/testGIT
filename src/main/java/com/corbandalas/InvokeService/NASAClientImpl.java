dc084bf865723542667a6ec4ed28f397397581bf
corbandalas <corban.dalas@me.com>
2022-12-13 18:10:59 +0300

    Add initial stuff for NASA client

--- src/main/java/com/corbandalas/InvokeService/NASAClientImpl.java
+++ src/main/java/com/corbandalas/InvokeService/NASAClientImpl.java
@@ -1,10 +1,14 @@
 package com.corbandalas.InvokeService;
 
 import java.util.List;
+import java.util.Objects;
 
 public class NASAClientImpl implements NASAClient {
     @Override
     public List<String> getPictures(String url) {
+
+        Objects.requireNonNull(url);
+
         return null;
     }
 }
